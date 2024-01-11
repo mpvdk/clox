201cdcb5c45de753692ef8ea89582296db5050bb
martijn <martijnvdkerkhof@gmail.com>
2024-01-11 18:12:04 +0100

    implement hash table

--- memory.c
+++ memory.c
@@ -11,12 +11,12 @@ static void freeObject(Obj* object)
     switch (object->type)
     {
         case OBJ_STRING:
-            {
-                ObjString* string = (ObjString*)object;
-                FREE_ARRAY(char, string->chars, string->length + 1);
-                FREE(ObjString, object);
-                break;
-            }
+        {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+            break;
+        }
     }
 }
 
